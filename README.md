94eb3ede101d7948322eea9543b48a7079c3881e
Narendra Sisodiya <narendra@narendrasisodiya.com>
2012-12-27 13:28:38 +0530

    split of public & private API of sandbox, added logging, Changed function name

--- README.md
+++ README.md
@@ -9,9 +9,137 @@ MIT: http://nsisodiya.mit-license.org
 
 Demo
 =============
-Visit https://github.com/nsisodiya/Demo-Scalable-App
-
+Visit http://nsisodiya.github.com/Demo-Scalable-App
 
 Dependancy
 ============
 http://amplifyjs.com/
+
+
+API
+====
+## Syntax - Choona.loadApplication
+
+```javascript
+	var <module-object> = new choona.loadApplication(<id-of-container>, <Application-Module>, <optional-configuration-object>);
+```
+### <id-of-container>
+Every module/application need a document node to load itself. All the oprations of module/application is restricteded to
+that container Node. First argument is id of container node.
+
+### <Application-Object>
+This is the Application itself. This is the starting point of the code.
+
+### <optional-configuration-object>
+This is an optional configuration argument. An Module can read its optinoal configuration from 
+```
+this.config
+```
+### Return
+Constructor return <module>. <module> has its API.
+
+## Module-object API
+
+### Start function
+```javascript
+<module-object>.start();
+```
+This is start the application.
+
+### End function
+```javascript
+<module-object>.end();
+```
+This is end the application.
+### Example
+
+```javascript
+	var module1 = new choona.loadApplication("applicationContainer", {
+		start : function(){
+			$(this.$).append("<p>THIS IS HEADER PANEL</p>");
+	
+		},
+		end: function(){
+	
+		}
+	});
+	module1.start();
+	module1.end();
+```
+
+You can also split this into multiple files like
+```javascript
+	//File1.js
+	var helloWorldApp = {
+		start : function(){
+			$(this.$).append("<p>THIS IS HEADER PANEL</p>");
+	
+		},
+		end: function(){
+	
+		}
+	}
+	//File2.js
+	var module1 = new choona.loadApplication("applicationContainer", helloWorldApp);
+	module1.start();
+	module1.end();
+```
+## Syntax - Sandbox API
+Every Application Module can load multiple modules. Technically there is no difference between
+an Applicaton or module or chile module.
+Every Module cantains a SANBOX. Every Module can load multiple child modules. 
+Every Module can delete its child modules.
+
+How its all happen ? Sanbox is the toolkit of a module. SANBOX provide useful 
+methods to load/unload new child modules. One module talk to another using SANBOX.
+
+## What is this.$ inside a module ??
+
+Here it is. Every Module is GIFTED with following variable.
+
+id, $, sb, config
+
+
+```
+*	this.id => Id of module.  <id-of-container> Ex -- "applicationContainer"
+*	this.$ => Dom Element == document.getElementById(this.id);
+*	$(this.$) === $("#" + this.id) => jQueryfied of container node -- Ex $("#applicationContainer");
+*	this.config => Configuration - <optional-configuration-object>
+*	this.sb => instance of sandbox associated with module. It provide useful API
+```
+### this.sb.publish  ==> Send Signals
+```javascript
+	this.sb.publish(<Topic-Id>, <Object-to-be-published>);
+```
+
+Example
+
+```javascript
+	this.sb.publish("NameChanged", {newName:"Narendra"});
+```
+
+### this.sb.subscribe  ==> Recieve Signals
+```javascript
+	this.sb.subscribe(<Topic-Id>, <Fuction>);
+```
+
+Example
+
+```javascript
+	this.sb.subscribe("NameChanged", function(val){
+		alert("Your Name has been changed. New name is " + val.newName);
+	});
+```
+### this.sb.unsubscribe  ==> Stop recieving Signals
+```javascript
+	this.sb.unsubscribe(<Topic-Id>);
+```
+### this.sb.startModule  ==> Load a child module inside current module
+```javascript
+	var <module-object> = this.sb.startModule(<id-of-container>, <Child-Module>, <optional-configuration-object>);
+```
+### this.sb.endModule  ==> End a child module inside current module
+```javascript
+	var <module-object> = this.sb.endModule(<id-of-container>);
+```
+
--- choona.js
+++ choona.js
@@ -1,7 +1,7 @@
 //     choona.js 1.0.0
 
 //     (c) 2011-2012 Narendra Sisodiya, narendra@narendrasisodiya.com
-//     choona.js may be freely distributed under the MIT license.
+//     choona.js is distributed under the MIT license.
 //     For all details and documentation:
 //     https://github.com/nsisodiya/choona.js
 //     For demos using Choona.js
@@ -10,12 +10,55 @@
 
 var choona = (function(){
 
+	var Util = {
+		configuration : {},
+		log : function(msg) {
+			if (this.configuration.debug === true && typeof console == "object"
+					&& typeof console.log == "function") {
+				console.log(msg);
+			}
+		},
+
+		loadConfiguration : function(config) {
+			Util.configuration = config;
+		}
+
+	};
+	
+	
+	
+
 	var Sandbox = function(id){
 		this.id = id;
 		this.topicList = {};
-		this.moduleList = [];
-	}
+		this.moduleList = {};
+	};
 
+	Sandbox.Private = {
+		endAllModules: function(){
+			for (var i in this.moduleList ){
+				this.moduleList[i].end();
+			}
+			this.moduleList = {};
+		},
+		unsubscribeAll: function(){
+			for(var topic in this.topicList){
+				this.unsubscribe(topic);
+			}
+		},
+		eraseUI: function(){
+			document.getElementById(this.id).innerHTML = '';
+		},
+		getModule: function(id){
+			//Get Module by ID, From Module List
+			if(this.moduleList[id] === undefined){
+				throw new Error("id::" + id  + " is do not has any module. Please use, this.sb.startModule function to load module");
+			}else{
+				return this.moduleList[id];
+			}
+		}
+
+	};
 	Sandbox.prototype = {
 		subscribe : function(topic, callback){//publish
 			this.topicList[topic] = amplify.subscribe(topic, callback);
@@ -23,52 +66,44 @@ var choona = (function(){
 		},
 		unsubscribe : function(topic){
 			amplify.unsubscribe(topic, this.topicList[topic]);
+			delete this.topicList[topic];
+			Util.log('cleared topic -> ' + topic);
 		},
 		publish: function(topic, val){//public
 			amplify.publish(topic, val);
 		},
-		loadModule: function(id, creator, config){//public
-			this.moduleList.push(new AppCore(id, creator, config));
-			return this.moduleList[this.moduleList.length - 1];
-		},
-		
-		//@TODO - Remove it from prototype chain, user should not be able to access it
-		endAllModules: function(){
-			var l = this.moduleList.length;
-			for (var i = 0 ; i < l ; i++){
-				this.moduleList[i].end();
+		startModule: function(id, creator, config){//public
+			if(typeof id != "string" || id == ""){
+				throw new Error("Id provided is not String");
 			}
-			this.moduleList.length = 0;
-		},
-		
-		//@TODO - Remove it from prototype chain, user should not be able to access it
-		unsubscribeAll: function(){
-			for(var topic in this.topicList){
-				amplify.unsubscribe(topic, this.topicList[topic]);
-				//console.log('cleared topic -> ' + topic);
+			if(this.moduleList[id] === undefined){
+				//You cannot load more than 1 module at given Id.
+				//There is No module Loaded.
+				this.moduleList[id] = new AppCore(id, creator, config);
+				this.moduleList[id].start();
+			}else{
+				throw new Error("id::" + id  + " is already contains a module.  Please provide separate id new module");
 			}
 		},
-		//@TODO - Remove it from prototype chain, user should not be able to access it
-		eraseUI: function(){
-			//@TODO
-			document.getElementById(this.id).innerHTML = '';
+		endModule:function(id){
+			Sandbox.Private.getModule.call(this,id).end();
+			delete this.moduleList[id];
+			//Deletion is needed because if parent get Ended, it should not try to delete the module again.
 		}
-	}
-
-
+	};
 
 	var AppCore = function(id, protoObj_Module, config){
 	
 		var defaultCreator = function(sandbox, config){
 			this.sb = sandbox;
 			this.id = sandbox.id;			//Id of Container - This may be require to create Unique Ids
-			this.$ =  document.getElementById(this.id);		//Container of module @TODO - What if there are multiple ids ? ideally submodule must find DOM node inside Tree.
+			this.$ =  document.getElementById(this.id);		//Id must be Unique. You must load Unique ID for any module which has any UI element.
+			//@TODO - create a new ID if ID provided is Null or Undefined
 			this.config = config;
 		};
 		defaultCreator.prototype = protoObj_Module;
 		
-		this.sandbox = new Sandbox(id);
-		this.module = new defaultCreator( this.sandbox, config);
+		this.module = new defaultCreator( new Sandbox(id) , config);
 	
 	};
 
@@ -79,17 +114,17 @@ var choona = (function(){
 		},
 	
 		end: function(){
-			this.sandbox.endAllModules();
+			Sandbox.Private.endAllModules.call(this.module.sb);
 			this.module.end();
-			this.sandbox.eraseUI();
-			this.sandbox.unsubscribeAll();
-			//console.log('ended -> ', this.sandbox.getId());
+			Sandbox.Private.eraseUI.call(this.module.sb);
+			Sandbox.Private.unsubscribeAll.call(this.module.sb);
+			Util.log('ended -> ' +  this.module.id);
 		}
-		
 	};
 	
 	return {
-		loadApplication : AppCore
+		loadApplication : AppCore,
+		configure: Util.loadConfiguration
 	};
 
 })();
